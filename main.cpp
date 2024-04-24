9e15fb6e4592fc13d7c385d7e8a1f0efdf8570e2
bittiboop <sstrelkova15@gmail.com>
2024-04-24 18:56:21 +0300

    added ex 4

--- main.cpp
+++ main.cpp
@@ -2,7 +2,24 @@
 using namespace std;
 
 int main() {
+	int num1, num2;
+	cout << "enter first number:" << endl;
+	cin >> num1;
+	cout << "enter second number:" << endl;
+	cin >> num2;
 
-
+	if (num1 == num2)
+	{
+		cout << "number 2 and number 1 is equal!\nyappy!" << endl;
+	}
+	else 
+	{
+		if (num1 > num2) {
+			cout << num1 << "\n" << num2 << endl;
+		}
+		if (num1 < num2) {
+			cout << num2 << "\n" << num1 << endl;
+		}
+	}
 	return 0;
 }
