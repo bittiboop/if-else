e3522176bf69186f7ea0cad66d3df04d9d418e57
bittiboop <sstrelkova15@gmail.com>
2024-04-24 18:46:47 +0300

    added the second ex code

--- main.cpp
+++ main.cpp
@@ -2,9 +2,19 @@
 using namespace std;
 
 int main() {
-	int x;
-	cin >> x;
+	int num1;
+	cin >> num1;
+	int num2;
+	cin >> num2;
 
-	(x % 2 == 0) ? cout << "Even" : cout << "Odd";
+	if (num1 > num2) {
+		cout << "number two is less than first one" << endl;
+	}
+	else if (num2 > num1) {
+		cout << "number one is less than second one" << endl;
+	}
+	else {
+		cout << "they`re the same" << endl;
+	}
 	return 0;
 }
