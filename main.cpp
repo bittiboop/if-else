6a0b17cb4e433320bdcdc1bec2725af446f11d00
bittiboop <sstrelkova15@gmail.com>
2024-04-24 18:51:30 +0300

    added third ex code

--- main.cpp
+++ main.cpp
@@ -2,7 +2,17 @@
 using namespace std;
 
 int main() {
+	int num;
+	cin >> num;
 
-
+	if(num > 0) {
+		cout << "number is positive" << endl;
+	}
+	else if (num < 0) {
+		cout << "number is negative" << endl;
+	}
+	else {
+		cout << "number is equal to zero" << endl;
+	}
 	return 0;
 }
