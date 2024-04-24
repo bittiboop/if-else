cfe51dd26241d698811f0a6837b6f28e0b6a17ee
bittiboop <sstrelkova15@gmail.com>
2024-04-24 20:47:42 +0300

    added first base of code

--- main.cpp
+++ main.cpp
@@ -2,7 +2,22 @@
 using namespace std;
 
 int main() {
+	int planeBC, planeAB, cargoPlane;
+	int fuelPlane = 300;
 
+	cout << "Enter distance between point A and point B(km): ";
+	cin >> planeAB;
+	cout << "Enter distance between point B and point C(km): ";
+	cin >> planeBC;
+	cout << "Enter cargo weight in plane(kg): ";
+	cin >> cargoPlane;
 
+	if (cargoPlane > 2000)
+	{
+		cout << "The transportation is impossible, too much weight" << endl;
+	}
+	else {
+		
+	}
 	return 0;
 }
