b4945b013751f1a9f27e9e4f2240bca4d5c4a753
Strelkova Sofiya Vadimovna <156002431+bittiboop@users.noreply.github.com>
2024-06-16 01:21:35 +0300

    Update main.cpp
    updated code

--- main.cpp
+++ main.cpp
@@ -2,22 +2,49 @@
 using namespace std;
 
 int main() {
-	int planeBC, planeAB, cargoPlane;
-	int fuelPlane = 300;
+    double distance-AB, distance-BC;
+    int weight;
 
-	cout << "Enter distance between point A and point B(km): ";
-	cin >> planeAB;
-	cout << "Enter distance between point B and point C(km): ";
-	cin >> planeBC;
-	cout << "Enter cargo weight in plane(kg): ";
-	cin >> cargoPlane;
+    cout << "Введіть відстань між пунктами А і В: ";
+    cin >> distance-AB;
 
-	if (cargoPlane > 2000)
-	{
-		cout << "The transportation is impossible, too much weight" << endl;
-	}
-	else {
-		
-	}
-	return 0;
+    cout << "Введіть відстань між пунктами В і С: ";
+    cin >> distance-BC;
+
+    cout << "Enter the weight of the cargo: ";
+    cin >> weight;
+
+    int fuel-consumption;
+    if (weight <= 500) {
+        fuel-consumption = 1;
+    } else if (weight <= 1000) {
+        fuel-consumption = 4;
+    } else if (weight <= 1500) {
+        fuel-consumption = 7;
+    } else if (weight <= 2000) {
+        fuel-consumption = 9;
+    } else {
+        cout << "The plane does not lift a load of more than 2000 kg" << endl;
+        return 0;
+    }
+
+    double fuel-needed-AB = distance-AB * fuel-consumption;
+    double fuel-needed-BC = distance-BC * fuel-consumption;
+    if (fuel-needed-AB > 300) {
+        cout << "It`s impossible to overcome the distance between points A and B" << endl;
+        return 0;
+    }
+    if (fuel-needed-BC > 300) {
+        cout << "It`s impossible to overcome the distance between points B and C" << endl;
+        return 0;
+    }
+    double total-fuel-needed = fuel-needed-AB + fuel-needed-BC;
+    double refill-needed = total-fuel-needed - 300;
+    if (refill-needed < 0) {
+        refill-needed = 0;
+    }
+
+    cout << "The required amount of fuel for refueling at point B: " << refill-needed << " liters " << endl;
+
+    return 0;
 }
